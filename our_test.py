7e906cf1df61bccf578475df578577ae436828fb
Stephen Smith <stephensmith13424@gmail.com>
2021-02-13 12:47:17 -0800

    Integrated Initial State code into moisture sensor code

--- our_test.py
+++ our_test.py
@@ -1,5 +1,13 @@
 import RPi.GPIO as GPIO
-import time
+from ISStreamer.Streamer import Streamer
+from time import sleep
+
+ACCESS_KEY = "ist_qKI-afoTgpg8HUGD3eY_mnAlB1tmcuhk"
+BUCKET_KEY = "BAWCM9RFDKDF"
+BUCKET_NAME = "test initial state"
+
+# create a Streamer instance
+streamer = Streamer(bucket_name=BUCKET_NAME, bucket_key=BUCKET_KEY, access_key=ACCESS_KEY)
 
 channel = 11
 GPIO.setmode(GPIO.BOARD) # This sets the board numbering scheme (channel 17 for BCM)
@@ -8,8 +16,16 @@ GPIO.setup(channel, GPIO.IN) # We are saying that the channel is an input
 def callback(channel):
     if GPIO.input(channel):
         print("No water detected\n")
+        print("Sending to Initial State\n")
+        myString = "No water detected"
+        streamer.log("Message",myString)
+        print("Sent to IS\n")
     else:
         print("Water detected!\n")
+        print("Sending to Initial State\n")
+        myString = "Water detected!"
+        streamer.log("Message",myString)
+        print("Sent to IS\n")
 
 GPIO.add_event_detect(channel,GPIO.BOTH,bouncetime=300) # goes in a loop to determine changes in the edge
 GPIO.add_event_callback(channel,callback) # Adds a function to call when the event detect is called - this one is good because it won't miss a change in state
